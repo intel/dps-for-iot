68f5cc057fff6ca3b2d3c178b2b6d195a8d5fa8d
Burns, Gregory <gregory.burns@intel.com>
2016-07-14 15:35:08 -0700

    Further refinements to scalable test bench

--- test/subtree_sim.c
+++ test/subtree_sim.c
@@ -14,7 +14,7 @@ static int infixWildcards = 0;
 
 static char lineBuf[200];
 
-#define MAX_PUB_TOPICS     2
+#define MAX_PUB_TOPICS     8
 #define NUM_PUB_FORMATS    5
 
 static const char* pubFormats[NUM_PUB_FORMATS] = {
@@ -25,7 +25,7 @@ static const char* pubFormats[NUM_PUB_FORMATS] = {
     "%d/%d/%d/%d/%d"
 };
 
-#define MAX_SUB_TOPICS        4  /* Max topics per subscription */
+#define MAX_SUB_TOPICS        8  /* Max topics per subscription */
 #define NUM_SUB_FORMATS       9
 #define FIRST_INFIX_WILDCARD  7  /* First format string with infix wild card */
 
@@ -57,21 +57,25 @@ static void FreeTopics(char* topics[], size_t num)
     }
 }
 
-static size_t InitSub(DPS_BitVector* bv, char* topics[])
+static const char* UniqueSubTopic = "foo/bar/hello/world";
+static const char* UniquePubTopic = "foo/bar/hello/world";
+
+
+static size_t InitRandomSub(DPS_BitVector* bv, char* topics[])
 {
     DPS_Status ret;
     size_t i = 0;
-    size_t numSubs = 1 + (rand() % (MAX_SUB_TOPICS - 1));
+    size_t numSubs = 1 + (random() % (MAX_SUB_TOPICS - 1));
 
     DPS_BitVectorClear(bv);
 
     while (i < numSubs) {
-        int fmt = rand() % NUM_SUB_FORMATS;
+        int fmt = random() % NUM_SUB_FORMATS;
         if (fmt < FIRST_INFIX_WILDCARD || infixWildcards) {
-            int a = rand() % 4;
-            int b = rand() % 4;
-            int c = rand() % 4;
-            int d = rand() % 4;
+            int a = random() % 4;
+            int b = random() % 4;
+            int c = random() % 4;
+            int d = random() % 4;
             topics[i] = malloc(32);
             sprintf(topics[i], subFormats[fmt], a, b, c, d);
             ret = DPS_AddTopic(bv, topics[i], "/.", DPS_Sub);
@@ -82,11 +86,11 @@ static size_t InitSub(DPS_BitVector* bv, char* topics[])
     return numSubs;
 }
 
-static size_t InitPub(DPS_BitVector* bv, char* topics[])
+static size_t InitRandomPub(DPS_BitVector* bv, char* topics[])
 {
     DPS_Status ret;
     size_t i;
-    size_t numPubs = 1 + (rand() % (MAX_PUB_TOPICS - 1));
+    size_t numPubs = 1 + (random() % (MAX_PUB_TOPICS - 1));
 
     DPS_BitVectorClear(bv);
 
@@ -94,12 +98,12 @@ static size_t InitPub(DPS_BitVector* bv, char* topics[])
         /*
          * Build a random topic
          */
-        int fmt = rand() % NUM_PUB_FORMATS;
-        int a = rand() % 4;
-        int b = rand() % 4;
-        int c = rand() % 4;
-        int d = rand() % 4;
-        int e = rand() % 4;
+        int fmt = random() % NUM_PUB_FORMATS;
+        int a = random() % 4;
+        int b = random() % 4;
+        int c = random() % 4;
+        int d = random() % 4;
+        int e = random() % 4;
         topics[i] = malloc(32);
         sprintf(topics[i], pubFormats[fmt], a, b, c, d, e);
         ret = DPS_AddTopic(bv, topics[i], "/.", DPS_Pub);
@@ -108,135 +112,164 @@ static size_t InitPub(DPS_BitVector* bv, char* topics[])
     return numPubs;
 }
 
-#define MAX_LEAFS 5
+#define MIN(x, y)  ((x) < (y) ? (x) : (y))
 
-typedef struct _SubTopic {
+#define MAX_LEAFS 10
+
+typedef struct _SubNode {
     DPS_BitVector* interests;
     DPS_BitVector* needs;
     char* strings[MAX_SUB_TOPICS];
     size_t count;
     int expect;
-    int wastedPubs;
     int falseMatches;
     int trueMatches;
     int totalSubs;
     size_t numLeafs;
-    struct _SubTopic* leaf[1];
-} SubTopic;
+    struct _SubNode* leaf[1];
+} SubNode;
 
-static SubTopic* BuildTree(int depth)
+#define MAX_TREE_DEPTH 8
+
+static size_t trueTrace[MAX_TREE_DEPTH];
+static size_t falseTrace[MAX_TREE_DEPTH];
+static size_t numNodes[MAX_TREE_DEPTH];
+static size_t totalMsgs;
+
+static SubNode* BuildTree(int depth)
 {
     size_t i;
-    SubTopic* topic;
+    SubNode* node;
 
     if (depth > 0) {
-        int numLeafs = 1 + rand() % MAX_LEAFS;
-        topic = calloc(1, sizeof(SubTopic) + sizeof(struct _SubTopic*) * (numLeafs - 1));
+        int numLeafs = 1 + random() % MAX_LEAFS;
+        node = calloc(1, sizeof(SubNode) + sizeof(struct _SubNode*) * (numLeafs - 1));
         for (i = 0; i < numLeafs; ++i) {
-            topic->leaf[i] = BuildTree(depth - 1);
+            node->leaf[i] = BuildTree(depth - 1);
         }
-        topic->numLeafs = numLeafs;
+        node->numLeafs = numLeafs;
     } else {
-        topic = calloc(1, sizeof(SubTopic));
+        node = calloc(1, sizeof(SubNode));
     }
-    topic->interests = DPS_BitVectorAlloc();
-    return topic;
+    ++numNodes[depth];
+    node->interests = DPS_BitVectorAlloc();
+    return node;
 }
 
-static void FreeTree(SubTopic* topic)
+static void FreeTree(SubNode* node)
 {
     size_t i;
-    for (i = 0; i < topic->numLeafs; ++i) {
-        FreeTree(topic->leaf[i]);
+    for (i = 0; i < node->numLeafs; ++i) {
+        FreeTree(node->leaf[i]);
     }
-    DPS_BitVectorFree(topic->interests);
-    free(topic);
+    DPS_BitVectorFree(node->interests);
+    free(node);
 }
 
-static void ShowTree(SubTopic* topic, int depth)
+static void ShowTree(SubNode* node, int depth)
 {
     char in[64];
 
-    memset(in, topic->expect ? '+' : '-', depth * 2);
+    memset(in, node->expect ? '+' : '-', depth * 2);
     in[depth * 2] = 0;
-    if (topic->numLeafs > 0) {
+    if (node->numLeafs > 0) {
         size_t i;
-        DPS_PRINT("%s%d\n", in, topic->numLeafs);
-        for (i = 0; i < topic->numLeafs; ++i) {
-            ShowTree(topic->leaf[i], depth + 1);
+        DPS_PRINT("%s%d\n", in, node->numLeafs);
+        for (i = 0; i < node->numLeafs; ++i) {
+            ShowTree(node->leaf[i], depth + 1);
         }
     } else {
-        PrintTopics(in, topic->strings, topic->count);
+        PrintTopics(in, node->strings, node->count);
     }
 }
 
-static void CleanTree(SubTopic* topic)
+static void ReplaceSubscription(SubNode* node, const char* topicString)
+{
+    if (node->numLeafs) {
+        size_t i = random() % node->numLeafs;
+        ReplaceSubscription(node->leaf[i], topicString);
+        /*
+         * Need to rebuild the bit vector
+         */
+        DPS_BitVectorClear(node->interests);
+        node->needs = DPS_BitVectorWhiten(NULL);
+        for (i = 0; i < node->numLeafs; ++i) {
+            DPS_BitVectorUnion(node->interests, node->leaf[i]->interests);
+            DPS_BitVectorIntersection(node->needs, node->needs, node->leaf[i]->needs);
+        }
+    } else {
+        DPS_BitVectorClear(node->interests);
+        FreeTopics(node->strings, node->count);
+        node->strings[0] = strdup(topicString);
+        node->count = 1;
+        DPS_AddTopic(node->interests, node->strings[0], "/.", DPS_Sub);
+        node->needs = DPS_BitVectorWhiten(node->interests);
+    }
+}
+
+static void CleanTree(SubNode* node)
 {
     size_t i;
-    for (i = 0; i < topic->numLeafs; ++i) {
-        CleanTree(topic->leaf[i]);
+    for (i = 0; i < node->numLeafs; ++i) {
+        CleanTree(node->leaf[i]);
     }
-    DPS_BitVectorFree(topic->needs);
-    FreeTopics(topic->strings, topic->count);
+    FreeTopics(node->strings, node->count);
 }
 
-static void PopulateTree(SubTopic* topic)
+static void PopulateTree(SubNode* node)
 {
-    if (topic->numLeafs == 0) {
-        topic->count = InitSub(topic->interests, topic->strings);
-        topic->needs = DPS_BitVectorWhiten(topic->interests);
-        topic->totalSubs += topic->count;
+    if (node->numLeafs == 0) {
+        node->count = InitRandomSub(node->interests, node->strings);
+        node->needs = DPS_BitVectorWhiten(node->interests);
+        node->totalSubs += node->count;
     } else {
         size_t i;
-        DPS_BitVectorClear(topic->interests);
-        topic->needs = DPS_BitVectorWhiten(NULL);
-        for (i = 0; i < topic->numLeafs; ++i) {
-            PopulateTree(topic->leaf[i]);
-            DPS_BitVectorUnion(topic->interests, topic->leaf[i]->interests);
-            DPS_BitVectorIntersection(topic->needs, topic->needs, topic->leaf[i]->needs);
+        DPS_BitVectorClear(node->interests);
+        node->needs = DPS_BitVectorWhiten(NULL);
+        for (i = 0; i < node->numLeafs; ++i) {
+            PopulateTree(node->leaf[i]);
+            DPS_BitVectorUnion(node->interests, node->leaf[i]->interests);
+            DPS_BitVectorIntersection(node->needs, node->needs, node->leaf[i]->needs);
         }
     }
 }
 
-static int SetExpects(SubTopic* topic, char** pubs, size_t numPubs)
+static int SetExpects(SubNode* node, char** pubs, size_t numPubs)
 {
-    if (topic->numLeafs == 0) {
-        DPS_MatchTopicList(pubs, numPubs, topic->strings, topic->count, "/.", &topic->expect);
+    if (node->numLeafs == 0) {
+        DPS_MatchTopicList(pubs, numPubs, node->strings, node->count, "/.", &node->expect);
     } else {
         size_t i;
-        topic->expect = 0;
-        for (i = 0; i < topic->numLeafs; ++i) {
-            topic->expect |= SetExpects(topic->leaf[i], pubs, numPubs);
+        node->expect = 0;
+        for (i = 0; i < node->numLeafs; ++i) {
+            node->expect |= SetExpects(node->leaf[i], pubs, numPubs);
         }
     }
-    return topic->expect;
+    return node->expect;
 }
 
-static void PropogatePub(SubTopic* topic, DPS_BitVector* pub)
+static void PropagatePub(SubNode* node, DPS_BitVector* pub, int depth)
 {
     DPS_Status ret;
     int match;
 
-    //DPS_PRINT("Pub: "); DPS_BitVectorDump(pub, 0);
-    if (topic->numLeafs == 0) {
-        match = DPS_BitVectorIncludes(pub, topic->interests);
-        if (match) {
-            if (topic->expect) {
-                ++topic->trueMatches;
+    if (node->numLeafs == 0) {
+        if (DPS_BitVectorIncludes(pub, node->interests)) {
+            if (node->expect) {
+                ++node->trueMatches;
             } else {
-                ++topic->falseMatches;
+                ++node->falseMatches;
             }
         } else {
-            if (topic->expect) {
+            if (node->expect) {
                 DPS_PRINT("False negative at leaf\n");
             }
         }
     } else {
-        DPS_BitVector* provides;
-        DPS_BitVector* tmp = DPS_BitVectorAlloc();
         size_t i;
-        for (i = 0; i < topic->numLeafs; ++i) {
-            SubTopic* leaf = topic->leaf[i];
+        DPS_BitVector* tmp = DPS_BitVectorAlloc();
+        for (i = 0; i < node->numLeafs; ++i) {
+            SubNode* leaf = node->leaf[i];
             DPS_BitVector* provides;
             /*
              * Duplicates match logic from dps.c
@@ -246,13 +279,13 @@ static void PropogatePub(SubTopic* topic, DPS_BitVector* pub)
             provides = DPS_BitVectorWhiten(tmp);
             match = DPS_BitVectorIncludes(provides, leaf->needs);
             if (match) {
-                if (!leaf->expect) {
-                    /*
-                     * Count number of wasted pub forwards from this node
-                     */
-                    ++topic->wastedPubs;
+                if (leaf->expect) {
+                    ++trueTrace[depth];
+                } else {
+                    ++falseTrace[depth];
                 }
-                PropogatePub(leaf, tmp);
+                ++totalMsgs;
+                PropagatePub(leaf, tmp, depth + 1);
             } else {
                 if (leaf->expect) {
                     DPS_PRINT("False negative\n");
@@ -267,39 +300,46 @@ static void PropogatePub(SubTopic* topic, DPS_BitVector* pub)
 typedef struct {
     int trueMatches;
     int falseMatches;
-    int wastedPubs;
     int totalSubs;
-    int nodes;
+    int numNodes;
 } Stats;
 
-static void Analyze(SubTopic* topic, Stats* stats)
+static void Analyze(SubNode* node, Stats* stats)
 {
     size_t i;
 
-    ++stats->nodes;
-    stats->trueMatches += topic->trueMatches;
-    stats->falseMatches += topic->falseMatches;
-    stats->wastedPubs += topic->wastedPubs;
-    stats->totalSubs += topic->totalSubs;
-    for (i = 0; i < topic->numLeafs; ++i) {
-        Analyze(topic->leaf[i], stats);
+    ++stats->numNodes;
+    stats->trueMatches += node->trueMatches;
+    stats->falseMatches += node->falseMatches;
+    stats->totalSubs += node->totalSubs;
+    for (i = 0; i < node->numLeafs; ++i) {
+        Analyze(node->leaf[i], stats);
     }
 }
 
-static void RunSimulation(int runs, int numSubs, int pubIters)
+static void RunSimulation(int runs, int treeDepth, int pubIters)
 {
     DPS_Status ret;
+    int i;
     int r;
     DPS_BitVector* pub = DPS_BitVectorAlloc();
-    SubTopic* subscriptions;
-    int pubs = 0;
+    SubNode fakeRoot;
+    SubNode* subscriptions;
+    int numPubs = 0;
+    int minMsgs = 0;
     Stats stats;
     float maxLoad = 0.0;
     float totalLoad = 0.0;
 
-    subscriptions = BuildTree(numSubs);
+    subscriptions = BuildTree(treeDepth);
 
+    fakeRoot.numLeafs = 1;
+    fakeRoot.leaf[0] = subscriptions;
+
+#if 0
     PopulateTree(subscriptions);
+    ShowTree(subscriptions, 0);
+#endif
 
     for (r = 0; r < runs; ++r) {
         float lf;
@@ -319,17 +359,39 @@ static void RunSimulation(int runs, int numSubs, int pubIters)
          */
         for (p = 0; p < pubIters; ++p) {
             char* pubTopics[MAX_PUB_TOPICS];
-            size_t numPubTopics = InitPub(pub, pubTopics);
+            size_t numPubTopics = InitRandomPub(pub, pubTopics);
             /*
              * Identify expected matches
              */
             SetExpects(subscriptions, pubTopics, numPubTopics);
             /*
-             * Propogate the publication up the tree
+             * Propagate the publication up the tree
              */
-            PropogatePub(subscriptions, pub);
+            PropagatePub(&fakeRoot, pub, -1);
             FreeTopics(pubTopics, numPubTopics);
-            ++pubs;
+            ++numPubs;
+        }
+        /*
+         * Sends a single publication
+         */
+        if (pubIters == 0) {
+            size_t n;
+            char* pubTopics[MAX_PUB_TOPICS];
+            size_t numPubTopics = 1;
+
+            pubTopics[0] = strdup(UniquePubTopic);
+            DPS_BitVectorClear(pub);
+            DPS_AddTopic(pub, pubTopics[0], "/.", DPS_Pub);
+            for (n = 0; n < 20; ++n) {
+                ReplaceSubscription(subscriptions, UniqueSubTopic);
+            }
+            /*
+             * Identify expected matches
+             */
+            SetExpects(subscriptions, pubTopics, numPubTopics);
+            FreeTopics(pubTopics, numPubTopics);
+            PropagatePub(&fakeRoot, pub, -1);
+            ++numPubs;
         }
         /*
          * Done with these subscriptions
@@ -340,16 +402,38 @@ static void RunSimulation(int runs, int numSubs, int pubIters)
 
     memset(&stats, 0, sizeof(stats));
     Analyze(subscriptions, &stats);
-    DPS_PRINT("Nodes=%d, pubs=%d\n", stats.nodes, pubs);
-    DPS_PRINT("Max load=%2.3f%%, Avg load=%2.3f%%\n", maxLoad, totalLoad / runs);
-    DPS_PRINT("Subs=%d, Matches=%d, false matches=%d, wasted pubs=%d\n",
-            stats.totalSubs, stats.trueMatches, stats.falseMatches, stats.wastedPubs);
-    DPS_PRINT("Waste ratio=%2.2f%%\n", (float)(stats.wastedPubs * 100) / (float)(pubs * stats.nodes));
-
     FreeTree(subscriptions);
+
+    minMsgs = 0;
+    for (i = 0; i <= treeDepth; ++i) {
+        minMsgs += trueTrace[i];
+    }
+
+    DPS_PRINT("Nodes=%d, pubs=%d, actual msgs=%d, min msgs=%d\n", stats.numNodes, numPubs, totalMsgs, minMsgs);
+    DPS_PRINT("Max load=%2.3f%%, Avg load=%2.3f%%\n", maxLoad, totalLoad / runs);
+    DPS_PRINT("Subs=%d, Matches=%d, false matches=%d\n", stats.totalSubs, stats.trueMatches, stats.falseMatches);
+    DPS_PRINT("Redundant message ratio=%2.2f%%\n", (float)((totalMsgs - minMsgs) * 100) / (float)(totalMsgs));
+
+    DPS_PRINT("Node count:           ");
+    for (i = treeDepth; i >= 0; --i) {
+        DPS_PRINT(" %7d ", numNodes[i]);
+    }
+    DPS_PRINT("\n");
+
+    DPS_PRINT("True propagations:    ");
+    for (i = 0; i <= treeDepth; ++i) {
+        DPS_PRINT(" %7d ", trueTrace[i]);
+    }
+    DPS_PRINT("\n");
+
+    DPS_PRINT("False propagations:   ");
+    for (i = 0; i <= treeDepth; ++i) {
+        DPS_PRINT(" %7d ", falseTrace[i]);
+    }
+    DPS_PRINT("\n");
 }
 
-static int IntArg(char* opt, char*** argp, int* argcp, int* val, int min, int max)
+static int IntArg(char* opt, char*** argp, int* argcp, int* val, uint32_t min, uint32_t max)
 {
     char* p;
     char** arg = *argp;
@@ -379,9 +463,11 @@ int main(int argc, char** argv)
     int bitLen = 1024 * 16;
     int runs = 100;
     int whitening = 10;
-    int subLevels = 4;
+    int treeDepth = 4;
     int hashes = 4;
     int scaling = 32;
+    int seed = 0;
+    int pubs = 100;
     DPS_Status ret;
     char** arg = ++argv;
 
@@ -403,7 +489,7 @@ int main(int argc, char** argv)
             infixWildcards = 1;
             continue;
         }
-        if (IntArg("-l", &arg, &argc, &subLevels, 1, 8)) {
+        if (IntArg("-t", &arg, &argc, &treeDepth, 1, MAX_TREE_DEPTH)) {
             continue;
         }
         if (IntArg("-r", &arg, &argc, &runs, 1, 100000)) {
@@ -422,24 +508,36 @@ int main(int argc, char** argv)
         if (IntArg("-w", &arg, &argc, &whitening, 1, 60)) {
             continue;
         }
+        if (IntArg("-s", &arg, &argc, &seed, 0, UINT32_MAX)) {
+            continue;
+        }
+        if (IntArg("-p", &arg, &argc, &pubs, 0, UINT32_MAX)) {
+            continue;
+        }
         goto Usage;
+    }
+
+    if (seed) {
+        srandom(seed);
+    } else {
+        struct timespec t;
+        clock_gettime(CLOCK_MONOTONIC, &t);
+        srandom(t.tv_nsec);
     }
 
     if (DPS_Configure(bitLen, hashes, scaling, whitening) != DPS_OK) {
         DPS_PRINT("Invalid configuration parameters\n");
         return 1;
     }
-    DPS_PRINT("Bit length=%d (%d bytes) whitened length=%d\n", bitLen, bitLen / 8, bitLen / scaling);
+    DPS_PRINT("Bit length=%d (%d bytes)\n", bitLen, bitLen / 8);
 
-    RunSimulation(runs, subLevels, 100);
+    RunSimulation(runs, treeDepth, pubs);
     return 0;
 
 Usage:
-    DPS_PRINT("Usage %s [-i] [-v] [-l <levels>] [b <bits> | -k <kbits>] [-r <runs>] [-d]\n", *argv);
+    DPS_PRINT("Usage %s [-i] [-v] [-t <tree depth>] [b <bits> | -k <kbits>] [-r <runs>] [-d]\n", *argv);
     DPS_PRINT("      -i   Enable infix wildcards\n");
     DPS_PRINT("      -v   Verbose\n");
     DPS_PRINT("      -d   Debugging\n");
     return 1;
 }
-
-
