7368c00dce08848d4e609b7c2a9aa523e11d9d69
Burns, Gregory <gregory.burns@intel.com>
2017-02-02 09:11:58 -0800

    Fix graphing tool

--- tools/dps_graph.pl
+++ tools/dps_graph.pl
@@ -4,18 +4,19 @@ open(SS, "ss -p -a |");
 
 while ( <SS> )
 {
-    if (/ESTAB.*reg_subs/) {
+    if (/ESTAB.*reg_subs/ || /ESTAB.*subscriber/) {
         my @elem = split(/ +/);
         my ($addr, $localPort) = @elem[4] =~ /([0-9,a-f,A-F,:]+):([0-9]+)$/;
         my ($addr, $peerPort) = @elem[5] =~ /([0-9,a-f,A-F,:]+):([0-9]+)$/;
         my ($pre, $pid, $post) =  @elem[6] =~ /(.*pid=)([0-9]+)(.*)/;
         push(@{$links{$pid}}, $peerPort);
     }
-    if (/LISTEN.*reg_subs/) {
+    if (/LISTEN.*reg_subs/ || /LISTEN.*subscriber/) {
         my @elem = split(/ +/);
         my ($addr, $listenPort) = @elem[4] =~ /([0-9,a-f,A-F,:]+):([0-9]+)$/;
         my ($pre, $pid, $post) =  @elem[6] =~ /(.*pid=)([0-9]+)(.*)/;
         $nodes{$listenPort} = $pid;
+        push(@{$listener{$pid}}, $listenPort);
     }
 }
 
@@ -24,7 +25,12 @@ foreach $pid (keys %links)
 {
     foreach $port (@{$links{$pid}}) {
         if (exists($nodes{$port})) {
-            print "    $pid -> $nodes{$port};";
+            $label = "@{$listener{$pid}}";
+            if ($label == "") {
+                $label = "N/A";
+            }
+            print "    $pid [label=\"$label\"];";
+            print "    $pid -> $nodes{$port}";
         }
     }
     print "\n";
