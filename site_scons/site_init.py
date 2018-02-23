5bfa82caf515db53986ad558bc64f8def5df6066
Todd Malsbary <todd.malsbary@intel.com>
2018-02-23 15:42:09 -0800

    Use source path correctly in scons builder.
    Signed-off-by: Todd Malsbary <todd.malsbary@intel.com>

--- site_scons/site_init.py
+++ site_scons/site_init.py
@@ -6,7 +6,7 @@ import swig_doc
 def build_function(target, source, env):
     for t in target:
         if "py" in str(t):
-            swig_doc.generate("py", str(source[0]), str(t))
+            swig_doc.generate("py", source[0].srcnode().path, t.path)
         elif "js" in str(t):
-            swig_doc.generate("js", str(source[0]), str(t))
+            swig_doc.generate("js", source[0].srcnode().path, t.path)
     return None
