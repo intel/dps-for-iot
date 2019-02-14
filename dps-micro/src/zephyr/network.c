49b1a7a9caa40ef17161067bfc5561231ef27f30
Greg Burns <gregory.burns@intel.com>
2019-02-14 15:53:47 -0800

    Update network code for Zephyr
    Signed-off-by: Greg Burns <gregory.burns@intel.com>

--- dps-micro/src/zephyr/network.c
+++ dps-micro/src/zephyr/network.c
@@ -57,12 +57,78 @@ DPS_DEBUG_CONTROL(DPS_DEBUG_ON);
 #define IPV4  0
 #define IPV6  1
 
+struct _DPS_NodeAddress {
+    struct sockaddr_storage inaddr;
+};
+
+void DPS_NodeAddressSetPort(DPS_NodeAddress* addr, uint16_t port)
+{
+    port = htons(port);
+    if (addr->inaddr.ss_family == AF_INET6) {
+        struct sockaddr_in6* sa6 = (struct sockaddr_in6*)&addr->inaddr;
+        sa6->sin6_port = port;
+    } else {
+        struct sockaddr_in* sa4 = (struct sockaddr_in*)&addr->inaddr;
+        sa4->sin_port = port;
+    }
+}
+
+DPS_NodeAddress* DPS_AllocNodeAddress(DPS_AllocPool pool)
+{
+    return DPS_Calloc(sizeof(DPS_NodeAddress), pool);
+}
+
+void DPS_CopyNodeAddress(DPS_NodeAddress* dest, const DPS_NodeAddress* src)
+{
+    memcpy(dest, src, sizeof(DPS_NodeAddress));
+}
+
+static const uint8_t IP4as6[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xFF, 0xFF, 0, 0, 0, 0 };
+
+int DPS_SameNodeAddress(const DPS_NodeAddress* addr1, const DPS_NodeAddress* addr2)
+{
+    const struct sockaddr* a = (const struct sockaddr*)&addr1->inaddr;
+    const struct sockaddr* b = (const struct sockaddr*)&addr2->inaddr;
+    struct sockaddr_in6 tmp;
+
+    if (a->sa_family != b->sa_family) {
+        uint32_t ip;
+        tmp.sin6_family = AF_INET6;
+        if (a->sa_family == AF_INET6) {
+            const struct sockaddr_in* ipb = (const struct sockaddr_in*)b;
+            tmp.sin6_port = ipb->sin_port;
+            ip = ipb->sin_addr.s_addr;
+        } else {
+            const struct sockaddr_in* ipa = (const struct sockaddr_in*)a;
+            tmp.sin6_port = ipa->sin_port;
+            ip = ipa->sin_addr.s_addr;
+        }
+        memcpy(&tmp.sin6_addr, IP4as6, 12);
+        memcpy((uint8_t*)&tmp.sin6_addr + 12, &ip, 4);
+        if (a->sa_family == AF_INET6) {
+            b = (const struct sockaddr*)&tmp;
+        } else {
+            a = (const struct sockaddr*)&tmp;
+        }
+    }
+    if (a->sa_family == AF_INET6 && b->sa_family == AF_INET6) {
+        const struct sockaddr_in6* ip6a = (const struct sockaddr_in6*)a;
+        const struct sockaddr_in6* ip6b = (const struct sockaddr_in6*)b;
+        return (ip6a->sin6_port == ip6b->sin6_port) && (memcmp(&ip6a->sin6_addr, &ip6b->sin6_addr, 16) == 0);
+    } else if (a->sa_family == AF_INET && b->sa_family == AF_INET) {
+        const struct sockaddr_in* ipa = (const struct sockaddr_in*)a;
+        const struct sockaddr_in* ipb = (const struct sockaddr_in*)b;
+        return (ipa->sin_port == ipb->sin_port) && (ipa->sin_addr.s_addr == ipb->sin_addr.s_addr);
+    } else {
+        return DPS_FALSE;
+    }
+}
+
 struct _DPS_Network {
     struct net_context* context4;       /* Zephyr IPv4 network context */
     struct net_context* context6;       /* Zephyr IPv6 network context */
     uint8_t rxBuffer[RX_BUFFER_SIZE]; 
     size_t txLen;
-    DPS_OnReceive mcastRecvCB;
     DPS_OnReceive recvCB;
     struct sockaddr_in addrCOAP4;
     struct sockaddr_in6 addrCOAP6;
@@ -173,13 +239,19 @@ static DPS_Status JoinMCastGroup()
 	return DPS_OK;
 }
 
-static void OnData(struct net_context* context, struct net_pkt* pkt, int status, void* userData)
+static void OnData(struct net_context* context,
+                   struct net_pkt* pkt,
+                   union net_ip_header *ip_hdr,
+				   union net_proto_header *proto_hdr,
+                   int status,
+                   void* userData)
 {
     DPS_Node* node = (DPS_Node*)userData;
     DPS_Network* net;
     unsigned int hdrLen;
     unsigned int rcvLen;
     DPS_RxBuffer rxBuf;
+    DPS_NodeAddress from;
 
     DPS_DBGTRACE();
 
@@ -197,15 +269,28 @@ static void OnData(struct net_context* context, struct net_pkt* pkt, int status,
 		DPS_DBGPRINT("Packet to large for receive buffer\n");
         return;
 	}
-	net_frag_linearize(net->rxBuffer, rcvLen, pkt, hdrLen, rcvLen);
+	net_buf_linearize(net->rxBuffer, rcvLen, pkt->buffer, hdrLen, rcvLen);
+    /* Extract the source address and port from the headers */
+    memset(&from, 0, sizeof(from));
+    if (net_pkt_family(pkt) == AF_INET6) {
+        struct sockaddr_in6* sa6 = (struct sockaddr_in6*)&from.inaddr;
+        memcpy(&sa6->sin6_addr, &ip_hdr->ipv6->src, sizeof(struct in6_addr));
+        sa6->sin6_family = AF_INET6;
+        sa6->sin6_port = proto_hdr->udp->src_port;
+    } else {
+        struct sockaddr_in* sa4 = (struct sockaddr_in*)&from.inaddr;
+        memcpy(&sa4->sin_addr, &ip_hdr->ipv4->src, sizeof(struct in_addr));
+        sa4->sin_family = AF_INET;
+        sa4->sin_port = proto_hdr->udp->src_port;
+    }
     /* We are done with the packet so unref it so it can be freed */
     net_pkt_unref(pkt);
 
     DPS_RxBufferInit(&rxBuf, net->rxBuffer, rcvLen);
-    net->mcastRecvCB(node, &rxBuf, DPS_OK);
+    net->recvCB(node, &from, DPS_TRUE, &rxBuf, DPS_OK);
 }
 
-DPS_Status DPS_MCastStart(DPS_Node* node, DPS_OnReceive cb)
+DPS_Status DPS_NetworkStart(DPS_Node* node, DPS_OnReceive cb)
 {
     int ret;
     DPS_Status status;
@@ -221,7 +306,7 @@ DPS_Status DPS_MCastStart(DPS_Node* node, DPS_OnReceive cb)
     if (status != DPS_OK) {
         goto Exit;
     }
-    network->mcastRecvCB = cb;
+    network->recvCB = cb;
 
 	ret = net_context_recv(network->context6, OnData, K_NO_WAIT, node);
 	if (ret) {
@@ -265,6 +350,36 @@ DPS_Status DPS_MCastSend(DPS_Node* node, void* appCtx, DPS_SendComplete sendComp
     }
     /* TODO - register completion callback */
     ret = net_context_sendto(pkt, (struct sockaddr*)&addr6, sizeof(addr6), NULL, K_FOREVER, NULL, node);
+    if (ret < 0) {
+        /* balances the internal add ref inside net_pkt_get_tx() */
+        net_pkt_unref(pkt);
+        return DPS_ERR_NETWORK;
+    }
+	return DPS_OK;
+}
+
+DPS_Status DPS_UnicastSend(DPS_Node* node, DPS_NodeAddress* dest, void* appCtx, DPS_SendComplete sendCompleteCB)
+{
+    DPS_Network* net = node->network;
+    int ret;
+    unsigned int len = (unsigned int)(node->txLen + node->txHdrLen);
+    uint8_t* buf = node->txBuffer + DPS_TX_HEADER_SIZE - node->txHdrLen;
+    struct net_pkt* pkt;
+
+    DPS_DBGTRACE();
+
+    pkt = net_pkt_get_tx(net->context6, K_FOREVER);
+    if (!pkt) {
+        return DPS_ERR_NETWORK;
+    }
+    len = net_pkt_append(pkt, len, buf, K_FOREVER);
+    if (!len) {
+        /* balances the internal add ref inside net_pkt_get_tx() */
+        net_pkt_unref(pkt);
+        return DPS_ERR_NETWORK;
+    }
+    /* TODO - register completion callback */
+    ret = net_context_sendto(pkt, (struct sockaddr*)dest, sizeof(struct sockaddr_storage), NULL, K_FOREVER, NULL, node);
     if (ret < 0) {
         /* balances the internal add ref inside net_pkt_get_tx() */
         net_pkt_unref(pkt);
