32d5487ca563a717fed41d4ca76712b1f43a2a91
Todd Malsbary <todd.malsbary@intel.com>
2019-10-30 09:44:07 -0700

    Fix memory leak in subscriber example.
    Signed-off-by: Todd Malsbary <todd.malsbary@intel.com>

--- examples/subscriber.c
+++ examples/subscriber.c
@@ -294,7 +294,7 @@ static int LinkTo(Subscriber* subscriber, Args* args)
         }
     }
     if (ret == DPS_OK) {
-        subscriber->numAddrs += j;
+        subscriber->numAddrs = j;
         return DPS_TRUE;
     } else {
         DPS_ERRPRINT("DPS_Link %s returned %s\n", args->linkText[i], DPS_ErrTxt(ret));
@@ -335,6 +335,7 @@ static void ReadStdin(Subscriber* subscriber)
         }
         Subscribe(subscriber, &args);
         LinkTo(subscriber, &args);
+        DestroyLinkArg(args.linkText, NULL, args.numLinks);
     }
 }
 
